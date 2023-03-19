82a975c61986f101610e84ab463722e852057bab
Orrane Reid <rainydayscholar21@gmail.com>
2023-03-18 23:14:33 -0700

    test commit

--- UDPServer.py
+++ UDPServer.py
@@ -4,9 +4,13 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 count=0
+recFile = open("temp.txt", "wb")
+message, clientAddress = serverSocket.recvfrom(1024)
 while True:
+	#modifiedMessage = message.decode()
+	recFile.write(message)
+	serverSocket.settimeout(3)
 	message, clientAddress = serverSocket.recvfrom(1024)
-	modifiedMessage = message.decode()
-	count+=1
-	print(count)
-	serverSocket.sendto(modifiedMessage.encode(),clientAddress)	
+
+recFile.close()
+serverSocket.sendto(modifiedMessage.encode(),clientAddress)	
