0ef00285c96c234af1e1c807b66c32db5aed4bd2
Orrane Reid <rainydayscholar21@gmail.com>
2023-03-19 15:31:20 -0700

    includedwc functionality for all 3

--- UDPServer.py
+++ UDPServer.py
@@ -6,7 +6,9 @@ print("The server is ready to receive")
 count=0
 while True:
 	message, clientAddress = serverSocket.recvfrom(1024)
-	modifiedMessage = message.decode()
-	count+=1
-	print(count)
-	serverSocket.sendto(modifiedMessage.encode(),clientAddress)	
+	modifiedMessage = message.decode().lower()
+	lines = "Lines: " + message.decode().count('\n') + '\n'
+	words = "Words: " + str(len(message.decode().split(' '))) + '\n'
+	char = "Characters: " + str(len(message.decode())/4) + '\n'
+	finalMessage = lines + words + char + modifiedMessage
+	serverSocket.sendto(finalMessage.encode(),clientAddress)	
